506dfccf7ceaef1b93d8db772691fafff4d3a4fe
iitsTom <86893233+iitsTom@users.noreply.github.com>
2023-06-06 17:14:55 +0200

    Animacja i w sumie tyle

---
+++ employee-mainpage.js
@@ -0,0 +1,36 @@
+// let mainWindow = document.querySelector("#businesses");
+let track = document.querySelector("#imageTrack");
+
+window.onmousedown = e => {
+    track.dataset.mouseDownAt = e.clientX;
+}
+
+window.onmouseup = e => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage
+}
+
+window.onmousemove = e => {
+    if(track.dataset.mouseDownAt == "0") return;
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX, 
+          maxDelta = window.innerWidth/2;
+
+    const percentage = (mouseDelta / maxDelta) * -100,
+          nextPercentageTemp = parseFloat(track.dataset.prevPercentage) + percentage,
+          nextPercentage = Math.max(Math.min(nextPercentageTemp, 0), -100);
+
+    track.dataset.percentage = nextPercentage;
+
+    // track.style.transform = "translate(" + nextPercentage + "%, 20%)";
+    track.animate({
+        transform: `translate(${nextPercentage}%, 20%)`
+    }, {duration: 1200, fill:"forwards"});
+
+    for(const image of track.getElementsByClassName("image")){
+        // image.style.objectPosition = (nextPercentage + 100) + "50%";
+        image.animate({
+            objectPosition: `${nextPercentage + 100}% center`
+        }, {duration: 1200, fill:"forwards"});
+
+    }
+}
