a822aad6028372e9cc929eb051af2588ae0fed40
Siwek9 <72036867+Siwek9@users.noreply.github.com>
2023-06-07 06:23:03 +0200

    name is working

--- join-us.js
+++ join-us.js
@@ -7,22 +7,35 @@ $(document).ready(function() {
     $("#name-form").on('input', function(e) {
         var getElement = $("#name-form");
         var getInputValue = getElement.children("input").val();
-        var getErrorElement = getElement.children(".js-error-message");
-        const regexMin = /^(?=.{3,}$)[a-zA-Z0-9_]+$/;
-        const regexMax = /^(?=.{,20}$)[a-zA-Z0-9_]+$/;
-
-        if (!regexMin.test(getInputValue)) {
-            getErrorElement.text("Hasło jest zbyt krótkie.");
+        // var getErrorElement = getElement.children(".js-error-message");
+        // const regexMin = /^(?=.{,2}$)[a-zA-Z0-9_]+$/;
+        // const regexMax = /^(?=.{21,}$)[a-zA-Z0-9_]+$/;
+        if (getInputValue == "") {
+            HideFormError(getElement);
+        }
+        else if (getInputValue.length < 3) {
+            ShowFormError(getElement, "Nazwa użytkownika jest zbyt krótka.")
+        }
+        else if (getInputValue.length > 20){
+            ShowFormError(getElement, "Nazwa użytkownika jest zbyt długa.")
         }
         else {
-
+            HideFormError(getElement)
         }
 
     })
 });
 
 function ShowFormError(getElement, ErrorContent) {
-    getElement.children().text(ErrorContent);
+    getElement.children(".js-error-message").text(ErrorContent);
+    getElement.children(".warning-icon").css("display", "inline");
+    
+}
+
+
+function HideFormError(getElement) {
+    getElement.children(".js-error-message").text("");
+    getElement.children(".warning-icon").css("display", "none");
     
 }
 
@@ -35,6 +48,7 @@ function createUser(userData) {
         cache: false,
         processData: false,
         success: function(r) {
+            javascript:grecaptcha.reset($(".g-recaptcha")[0]);
             console.log(r);
             var result;
             try {
