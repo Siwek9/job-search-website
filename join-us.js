8c16eae8af022b3a00998f2dd957650b168406d1
Siwek9 <72036867+Siwek9@users.noreply.github.com>
2023-06-07 12:08:46 +0200

    almost finish validation

--- join-us.js
+++ join-us.js
@@ -4,26 +4,152 @@ $(document).ready(function() {
         createUser(this);
     });
 
-    $("#name-form").on('input', function(e) {
-        var getElement = $("#name-form");
-        var getInputValue = getElement.children("input").val();
-        // var getErrorElement = getElement.children(".js-error-message");
-        // const regexMin = /^(?=.{,2}$)[a-zA-Z0-9_]+$/;
-        // const regexMax = /^(?=.{21,}$)[a-zA-Z0-9_]+$/;
-        if (getInputValue == "") {
-            HideFormError(getElement);
-        }
-        else if (getInputValue.length < 3) {
-            ShowFormError(getElement, "Nazwa użytkownika jest zbyt krótka.")
-        }
-        else if (getInputValue.length > 20){
-            ShowFormError(getElement, "Nazwa użytkownika jest zbyt długa.")
-        }
-        else {
-            HideFormError(getElement)
-        }
+    $("#name-form").on({
+        'input': function(e) {
+            var htmlElement = $("#name-form");
+            var inputValue = htmlElement.children("input").val();
+            // var getErrorElement = getElement.children(".js-error-message");
 
-    })
+            var nameRegex = /^(?=.{3,20}$)[a-zA-Z0-9_]+$/;
+
+            if (inputValue == "") {
+                HideFormError(htmlElement);
+            }
+            else if (inputValue.length < 3) {
+                ShowFormError(htmlElement, "Nazwa użytkownika jest zbyt krótka.");
+            }
+            else if (inputValue.length > 20){
+                ShowFormError(htmlElement, "Nazwa użytkownika jest zbyt długa.");
+            }
+            else if (!nameRegex.test(inputValue)) {
+                ShowFormError(htmlElement, "Niedozwolone znaki w nazwie użytkownika.");
+            }
+            else {
+                HideFormError(htmlElement);
+            }
+
+        },
+        'change': function(e) {
+            // ajax
+        }
+    });
+
+    $("#email-form").on({
+        'input': function() {
+            var htmlElement = $("#email-form");
+            var inputValue = htmlElement.children("input").val();
+
+            var emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+
+            if (inputValue == "") {
+                HideFormError(htmlElement);
+            }
+            else if (!emailRegex.test(inputValue)) {
+                ShowFormError(htmlElement, "Niepoprawny Email.");
+            }
+            else {
+                HideFormError(htmlElement);
+            }
+        },
+        'change': function() {
+
+        }
+    });
+    
+    $("#password-form").on({
+        'input': function() {
+            var htmlElement = $("#password-form");
+            var htmlRepeatElement = $("#password-repeat-form");
+            
+            var passwordInput = $("#password-form").children("input").val();
+            var passwordRepeatInput = $("#password-repeat-form").children("input").val();
+            var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_-])[A-Za-z\d@$!%*?&_-]{8,20}$/;
+
+            if (passwordInput == "") {
+                HideFormError(htmlElement);
+                HideFormError(htmlRepeatElement);
+            }
+            else if (passwordInput.length < 8) {
+                ShowFormError(htmlElement, "Za krótkie hasło.");
+                HideFormError(htmlRepeatElement);
+            }
+            else if (passwordInput.length > 20) {
+                ShowFormError(htmlElement, "Za długie hasło.");
+                HideFormError(htmlRepeatElement);
+            }
+            else if (!passwordRegex.test(passwordInput)) {
+                ShowFormError(htmlElement, "Niepoprawne hasło.");
+                HideFormError(htmlRepeatElement);
+            }
+            else if (passwordInput != passwordRepeatInput && passwordRegex.test(passwordRepeatInput)) {
+                ShowFormError(htmlElement, "Podane hasła nie są identyczne.");
+                ShowFormError(htmlRepeatElement, "Podane hasła nie są identyczne.");
+            }
+            else {
+                HideFormError(htmlElement);
+                HideFormError(htmlRepeatElement);
+            }
+        },
+        'change': function() {
+            // var htmlElement = $("#password-form");
+            // var htmlRepeatElement = $("#password-repeat-form");
+            
+            // var passwordInput = $("#password-form").children("input").val();
+            // var passwordRepeatInput = $("#password-repeat-form").children("input").val();
+            // var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_-])[A-Za-z\d@$!%*?&_-]{8,20}$/;
+
+            // if (passwordRepeatInput != "" && passwordRegex.test(passwordInput) && passwordInput != passwordRepeatInput) {
+            //     ShowFormError(htmlElement, 'Hasła nie są identyczne');
+            //     ShowFormError(htmlRepeatElement, 'Hasła nie są identyczne');
+            // }
+            // else if (passwordRegex.test(passwordInput)) {
+            //     HideFormError(htmlElement);
+            //     HideFormError(htmlRepeatElement);
+            // }
+        }
+    });
+
+    $("#password-repeat-form").on({
+        'input': function() {
+            var htmlElement = $("#password-repeat-form");
+            var inputValue = htmlElement.children("input").val();
+            var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_-])[A-Za-z\d@$!%*?&_-]{8,20}$/;
+
+            if (inputValue == "") {
+                HideFormError(htmlElement);
+            }
+            else if (inputValue.length < 8) {
+                ShowFormError(htmlElement, "Za krótkie hasło.");
+            }
+            else if (inputValue.length > 20) {
+                ShowFormError(htmlElement, "Za długie hasło.");
+            }
+            else if (!passwordRegex.test(inputValue)) {
+                ShowFormError(htmlElement, "Niepoprawne hasło.");
+            }
+            else {
+                CheckValidForm();
+                HideFormError(htmlElement);
+            }
+        },
+        'change': function() {
+            var htmlElement = $("#password-form");
+            var htmlRepeatElement = $("#password-repeat-form");
+            
+            var passwordInput = $("#password-form").children("input").val();
+            var passwordRepeatInput = $("#password-repeat-form").children("input").val();
+            var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_-])[A-Za-z\d@$!%*?&_-]{8,20}$/;
+
+            if (passwordInput != "" && passwordRegex.test(passwordRepeatInput) && passwordInput != passwordRepeatInput) {
+                ShowFormError(htmlElement, 'Hasła nie są identyczne');
+                ShowFormError(htmlRepeatElement, 'Hasła nie są identyczne');
+            }
+            else if (passwordRegex.test(passwordRepeatInput)) {
+                HideFormError(htmlElement);
+                HideFormError(htmlRepeatElement);
+            }
+        }
+    });
 });
 
 function ShowFormError(getElement, ErrorContent) {
@@ -39,6 +165,10 @@ function HideFormError(getElement) {
     
 }
 
+function CheckValidForm() {
+    // if ()
+}
+
 function createUser(userData) {
     $.ajax({
         type: "POST",
@@ -48,7 +178,7 @@ function createUser(userData) {
         cache: false,
         processData: false,
         success: function(r) {
-            javascript:grecaptcha.reset($(".g-recaptcha")[0]);
+            grecaptcha.reset($(".g-recaptcha")[0]);
             console.log(r);
             var result;
             try {
