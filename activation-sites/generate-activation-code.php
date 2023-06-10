d15d2e7b5ffab735336cdb80685ae8a24e77ea42
Siwek9 <72036867+Siwek9@users.noreply.github.com>
2023-06-10 18:07:34 +0200

    emails work now

--- activation-sites/generate-activation-code.php
+++ activation-sites/generate-activation-code.php
@@ -8,67 +8,44 @@
         header("Location:../index.php");
         die;
     }
-
+    
     $userID = $_GET['userID'];
-
+    
     require('../server/database-functions.php');
-
+    
     $connect = database_connect_to_mysql();
-
+    
     if (!$connect) {
         header("Location:../index.php");
         die;
     }
 
-    $result 
-
-    // $stmt = $connect->prepare("SELECT email, id_user_data FROM accounts WHERE id = ?");
-    // $stmt->bind_param('i', $userID);
-    // $stmt->execute();
-
-    // $stmt->store_result();
+    $result = $connect->query("SELECT accounts.email AS email, account_activation.activation_code AS activation_code, account_activation.mail_sended AS mail_sended FROM accounts JOIN account_activation ON account_activation.user_id = accounts.id WHERE accounts.id = $userID");
     
-    // if ($stmt->num_rows() == 0) {
-    //     header("Location:../index.php");
-    //     die;
-    // } // check if user even exists
+    $data = $result->fetch_assoc();
+
+    if ($data['activation_code'] == null) {
+        header("Location:../index.php");
+        die;
+    }
+
+    $randomCode = $data['activation_code'];
     
-    // $stmt->execute();
-    // $result = $stmt->get_result();
-
-    // $data = $result->fetch_assoc(); // get user data
-
-    // $stmtIsCodeSended = $connect->prepare("SELECT * FROM account_activation WHERE user_id = ?");
-    // $stmtIsCodeSended->bind_param("i", $userID);
-    // $stmtIsCodeSended->execute();
-    // $stmtIsCodeSended->store_result();
-    // // check if code was generated
+    if (!$data["mail_sended"]) {
+        $result = $connect->query("UPDATE account_activation SET mail_sended = 1 WHERE user_id = $userID");
+        if ($result) {
+            require('../server/mail-functions.php');
+            sendMail($data['email'], "Aktywacja konta JobSE", require("activation-email.php"));
+        }
+    }
     
-    // // if no generate it and send it
-    
-    // if ($stmtIsCodeSended->num_rows() == 0) {
-    //     $randomCode = bin2hex(random_bytes(16));
-    //     $stmtCreateCode = $connect->prepare("INSERT INTO account_activation(user_id, activation_code, expiration_time) VALUES (?,?,DATE_ADD(NOW(), INTERVAL 2 MINUTE))");
-    //     $stmtCreateCode->bind_param('is', $userID, $randomCode);
-    //     $stmtCreateCode->execute();
-        
-    //     require('../server/mail-functions.php');
-    //     sendMail($data['email'], "Aktywacja konta JobSE", require("activation-email.php"));
-    // }
-    // else {
-    //     $stmtIsCodeSended->execute();
-    //     $result = $stmtIsCodeSended->get_result();
-
-    //     $randomCode = $result->fetch_assoc()['activation_code'];
-    // }
-
     // WARN: this is TEMPORARY for demo use because email don't want always to come to different email adressess than from 'onet.poczta.pl'
     // this should be deleted when own smtp server will be created
-    // echo "job-search-website/activation-sites/account-activated.php?userID=$userID&activationCode=$randomCode";
-
+    echo "job-search-website/activation-sites/account-activated.php?userID=$userID&activationCode=$randomCode";
+    
     // display website
     require_once('account-activation-prompt.html');
-
-
+    
+    
     
 ?>
